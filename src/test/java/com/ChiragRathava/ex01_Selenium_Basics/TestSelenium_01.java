8495d97f1c4bd15d70ae9ae5db3cd39d5086965e
ChiragRathava29 <rathavachirag29@gmail.com>
2025-05-19 16:32:07 +0530

    Part_1

---
+++ src/test/java/com/ChiragRathava/ex01_Selenium_Basics/TestSelenium_01.java
@@ -0,0 +1,16 @@
+package com.ChiragRathava.ex01_Selenium_Basics;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium_01 {
+
+        @Test
+        public void test_VerifyVWOLogin() {
+
+        // Write the code which will execute the perform the ui interactions
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+            System.out.println(driver.getTitle());
+    }
+}
